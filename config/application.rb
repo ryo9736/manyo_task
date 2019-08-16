8930f89a6c613ff9f54c3bfb3f761ed9fcb0c1c9
ryotaro yamashita <domdomgangan@gmail.com>
2019-08-16 21:56:09 +0900

    日本語設定、タイムゾーンを東京に設定

--- config/application.rb
+++ config/application.rb
@@ -1,5 +1,4 @@
 require_relative 'boot'
-
 require 'rails/all'
 
 Bundler.require(*Rails.groups)
@@ -7,6 +6,9 @@ Bundler.require(*Rails.groups)
 module ManyoTask
   class Application < Rails::Application
     config.load_defaults 5.2
+    config.time_zone = 'Tokyo'
+    config.active_record.default_timezone = :local
+    config.i18n.default_locale = :ja
     config.generators do |g|
       g.assets     false
       g.helper     false
