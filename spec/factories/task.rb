e8de32655292c224693e22d9dfd0e037646c2a8a
ryotaro yamashita <domdomgangan@gmail.com>
2019-08-17 19:59:31 +0900

    add:FactoryBot

---
+++ spec/factories/task.rb
@@ -0,0 +1,14 @@
+FactoryBot.define do
+
+    factory :task do
+      task_name { '課題1' }
+      content { 'テスト1' }
+      created_at{Time.zone.now}
+    end
+
+    factory :second_task, class: Task do
+      task_name { '課題2' }
+      content { 'テスト2' }
+      created_at{Time.zone.now+ 1.days}
+    end
+  end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -21,6 +21,8 @@ RSpec.configure do |config|
 
   config.filter_rails_from_backtrace!
 
+  config.include FactoryBot::Syntax::Methods
+
   config.before(:suite) do
     DatabaseCleaner.strategy = :truncation
   end
