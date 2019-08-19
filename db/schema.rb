9ea0b7574c134844223bcfe2d3ed85f69b769123
ryotaro yamashita <domdomgangan@gmail.com>
2019-08-19 20:13:06 +0900

    add:taskテーブルにsort_expiredを反映

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_17_120723) do
+ActiveRecord::Schema.define(version: 2019_08_19_014654) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 2019_08_17_120723) do
     t.text "content", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.datetime "sort_expired", default: "2019-08-19 20:02:55"
   end
 
 end
