19b9c839f77fdae479570ba04ef284bb268eee6d
ryotaro yamashita <domdomgangan@gmail.com>
2019-08-30 11:22:25 +0900

    add:"絞りこみ機能をスコープでメゾット定義"

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,4 +1,11 @@
 class Task < ApplicationRecord
-  validates :task_name, presence: true
+  validates :title,   presence: true
   validates :content, presence: true, length:{ maximum:100}
+
+  scope :expired , -> {order(sort_expired: "DESC")}
+  scope :recent , -> {order(created_at: "DESC")}
+  scope :search_title , -> (params) {where('(title LIKE?)',"%#{ params.dig(:task, :title)}%" )}
+  scope :search_status , -> (params) {where('(status LIKE?)',"%#{ params.dig(:task, :status) }%" )}
+
 end
+
