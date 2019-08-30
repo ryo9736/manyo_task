c6f1e8dfb431385f4d8d0a40ae02fc399a4d6e2f
ryotaro yamashita <domdomgangan@gmail.com>
2019-08-30 15:04:36 +0900

    add:"priorityモデルにenumを追加"

--- app/models/task.rb
+++ app/models/task.rb
@@ -2,10 +2,11 @@ class Task < ApplicationRecord
   validates :title,   presence: true
   validates :content, presence: true, length:{ maximum:100}
 
+  enum priority: { 高: 0, 中: 1, 低: 2 }
+
   scope :expired , -> {order(sort_expired: "DESC")}
   scope :recent , -> {order(created_at: "DESC")}
   scope :search_title , -> (params) {where('(title LIKE?)',"%#{ params.dig(:task, :title)}%" )}
   scope :search_status , -> (params) {where('(status LIKE?)',"%#{ params.dig(:task, :status) }%" )}
 
 end
-
