f1023c8e54f0d4dd069ed63bea998ffaef153ea2
ryotaro yamashita <domdomgangan@gmail.com>
2019-08-30 11:24:00 +0900

    add:"feature spec追記"

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -3,17 +3,18 @@ require 'rails_helper'
 RSpec.describe Task, type: :model do
 
     it "titleが空ならバリデーションが通らない" do
-      task = Task.new(task_name:'',content:'失敗テスト')
+      task = Task.new(title:'',content:'失敗テスト')
       expect(task).not_to be_valid
     end
 
     it "contentが空ならバリデーションが通らない" do
-      task = Task.new(task_name:'失敗テスト',content:'')
+      task = Task.new(title:'失敗テスト',content:'')
       expect(task).not_to be_valid
     end
 
     it "titleとcontentに内容が記載されていればバリデーションが通る" do
-      task = Task.new(task_name:'テスト１',content:'テスト１')
+      task = Task.new(title:'テスト１',content:'テスト１')
       expect(task).to be_valid
     end
+
   end
