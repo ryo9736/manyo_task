4b97db5bb6e77670e2b634b1c2e4df103d227ea4
ryotaro yamashita <domdomgangan@gmail.com>
2019-08-30 17:10:41 +0900

    add:"scope :priority_sort追加"

--- app/models/task.rb
+++ app/models/task.rb
@@ -4,7 +4,8 @@ class Task < ApplicationRecord
 
   enum priority: { 高: 0, 中: 1, 低: 2 }
 
-  scope :expired , -> {order(sort_expired: "DESC")}
+  scope :expired_sort , -> {order(expired: "DESC")}
+  scope :priority_sort , -> {order(priority: "ASC")}
   scope :recent , -> {order(created_at: "DESC")}
   scope :search_title , -> (params) {where('(title LIKE?)',"%#{ params.dig(:task, :title)}%" )}
   scope :search_status , -> (params) {where('(status LIKE?)',"%#{ params.dig(:task, :status) }%" )}
