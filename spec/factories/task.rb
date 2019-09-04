a47f09349a63a7928cda85d45683f55816154ac0
ryotaro yamashita <domdomgangan@gmail.com>
2019-09-04 18:01:22 +0900

    add:"user_id追加"

--- spec/factories/task.rb
+++ spec/factories/task.rb
@@ -7,15 +7,17 @@ FactoryBot.define do
       expired{Time.zone.now}
       status { '完了' }
       priority {'高'}
+      user_id { User.last.id }
     end
 
-    factory :second_task, class: Task do
+    factory :second_task , class: Task do
       title { '課題2' }
       content { 'テスト2' }
       created_at{Time.zone.now+ 1.days}
       expired{Time.zone.now + 1.days}
       status { '着手中'}
       priority {'中'}
+      user_id { User.last.id }
     end
 
 
