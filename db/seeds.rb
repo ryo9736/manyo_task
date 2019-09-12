50dbf438cf0ccd8479393df2a8365840e5a1106e
ryotaro yamashita <domdomgangan@gmail.com>
2019-09-12 17:28:33 +0900

    add:"seedデータ生成"

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,23 +1,11 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+User.create!(
+  name: '管理者ユーザー',
+  email: 'admin@test.com',
+  password: '111111',
+  admin: 'true'
+  )
 
-# 5.times do |n|
-#     name = Faker::Games::Pokemon.name
-#     email = Faker::Internet.email
-#     password = "password"
-#     User.create!(name: name,
-#                  email: email,
-#                  password: password,
-#                  password_confirmation: password,
-#                  )
-#   end
-
-10.times do |n|
+  10.times do |n|
     name = "ラベル#{n}"
   
     Label.create(
